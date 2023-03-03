dce35930eeb9e210a9e09f40a341a17b321131bd
Shubham <shubhamk209@gmail.com>
2023-03-04 01:08:42 +0530

    Added Category Repository Interface

---
+++ BuckyBookData/IRepository/Repository/ICategoryRespository.cs
@@ -0,0 +1,17 @@
+﻿using BuckyBookWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuckyBookDataAccess.IRepository.Repository
+{
+    internal interface ICategoryRespository : IRepository<Category>
+    {
+        // to update the category we implemnt the this method.
+        void Update(Category obj);
+        //to explicitly call when we want to save the changes to DB.
+        void Save();
+    }
+}
